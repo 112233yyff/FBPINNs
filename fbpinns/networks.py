fa1fd3dcc5a1072f4606d41cc40e9a9cb1fd4af7
112233yyff <1811731941@qq.com>
2024-03-08 17:13:06 +0800

    修改了部分函数

--- fbpinns/networks.py
+++ fbpinns/networks.py
@@ -101,8 +101,8 @@ class FCN(Network):
         keys = random.split(key, len(layer_sizes)-1)
         params = [FCN._random_layer_params(k, m, n)
                 for k, m, n in zip(keys, layer_sizes[:-1], layer_sizes[1:])]
-        trainable_params = {'layers': params}
-        # trainable_params =  params
+        # trainable_params = {'layers': params}
+        trainable_params =  params
         return {}, trainable_params
 
     @staticmethod
@@ -118,28 +118,28 @@ class FCN(Network):
         return w, b
 
     @staticmethod
-    def network_fn(params, x):
-        params = params["trainable"]["network"]["subdomain"]["layers"]
-        for w, b in params[:-1]:
-            x = jnp.dot(w, x) + b
-            x = jnp.tanh(x)
-        w, b = params[-1]
-        x = jnp.dot(w, x) + b
-        return x
-
     # def network_fn(params, x):
     #     params = params["trainable"]["network"]["subdomain"]["layers"]
-    #     for domain_params in params:
-    #         # 处理除了最后一层以外的其他层
-    #         for layer_params in domain_params[:-1]:
-    #             w, b = layer_params
-    #             x = jnp.dot(w, x) + b
-    #             x = jnp.tanh(x)
-    #     # 统一处理所有子域的最后一层
-    #     for domain_params in params:
-    #         w, b = domain_params[-1]
+    #     for w, b in params[:-1]:
     #         x = jnp.dot(w, x) + b
+    #         x = jnp.tanh(x)
+    #     w, b = params[-1]
+    #     x = jnp.dot(w, x) + b
     #     return x
+
+    def network_fn(params, x):
+        params = params["trainable"]["network"]["subdomain"]["layers"]
+        for domain_params in params:
+            # 处理除了最后一层以外的其他层
+            for layer_params in domain_params[:-1]:
+                w, b = layer_params
+                x = jnp.dot(w, x) + b
+                x = jnp.tanh(x)
+        # 统一处理所有子域的最后一层
+        for domain_params in params:
+            w, b = domain_params[-1]
+            x = jnp.dot(w, x) + b
+        return x
 
 
 class AdaptiveFCN(Network):
